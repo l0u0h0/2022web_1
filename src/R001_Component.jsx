2426b73782609982fa3c5ea0f9e767b3aeae32e1
l0u0h0 <l0u0h0@gmail.com>
2022-03-03 18:53:49 +0900

    project start

---
+++ src/R001_Component.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class R001_ImportComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h2 className='r001-title'>참고 사이트</h2>
+        <a className='r001-link'>npm</a>
+        <a className='r001-link'>repl.it</a>
+      </div>
+    );
+  }
+}
+
+export default R001_ImportComponent;
---
+++ src/R002_Component.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class R002_ImportComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h2 className='r002-title'>첫번째 실습</h2>
+        <p className='r002-script'>
+          이 모양을 만든 후 src폴더만 압축하여 사이버캠퍼스 과제게시판에 올려 주세요<br />
+          다른 사람의 것을 보고 하면 안됩니다.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default R002_ImportComponent;
