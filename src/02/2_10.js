fb3368c4918b201634715ade6576c3847720daea
l0u0h0 <l0u0h0@gmail.com>
2022-03-09 22:27:42 +0900

    2weeks ES6_2.2

--- src/02/2_10.js
+++ src/02/2_10.js
@@ -1,4 +1,5 @@
-function Shape(x, y) { // 생성자 형태로 선언 후
+function Shape(x, y) { 
+  // 생성자 형태로 선언 후
   this.name = 'Shape';
   this.move(x, y);
 }
@@ -7,7 +8,6 @@ function Shape(x, y) { // 생성자 형태로 선언 후
 Shape.create = function(x, y) {
   return new Shape(x, y);
 }
-
 // instance 함수 선언
 Shape.prototype.move = function(x, y) {
   this.x = x;
@@ -30,4 +30,22 @@ Shape.prototype = {
 // 변수와 함수를 prototype 객체에 할당
 var s = new Shape(0, 0);
 var s2 = Shape.create(0, 0);
-s.area(); // 0
+console.log(s.name, s.area()); // 0
+
+// 자식 클래스 생성
+function Circle(x, y, radius) {
+  // 부모클래스 생성자 호출 시 내장함수 call 이용, 이때 초기값 상송
+  Shape.call(this, x, y);
+  this.name = 'Circle';
+  this.radius = radius;
+}
+Object.assign(Circle.prototype, Shape.prototype, {
+  // 부모클래스의 함수 상송
+  area: function() {
+    // area 함수는 오버라이드
+    return this.radius * this.radius;
+  }
+});
+
+var c = new Circle(0, 0, 10);
+console.log(c.name, c.area()); // 100
