ce10b226454522b0d745b40d0f980fc44363dc90
l0u0h0 <l0u0h0@gmail.com>
2022-03-26 18:59:46 +0900

    success

--- src/04/4_1.js
+++ src/04/4_1.js
@@ -12,46 +12,21 @@ const peoples = [
     age: 20,
   },
 ];
-console.log(peoples[1].age);
 function parsing() {
   return peoples
     .map((people) => {
-      let { name, age } = people;
+      const { name, age } = people;
       return { name, age };
     })
     .reduce((result, item) => {
-      console.log(item.age);
-      console.log(result.age);
-      console.log(result.key);
-
-      if (item.age === result) {
-        console.log("hi");
-        return result.push(item.name);
-      } else {
-        return { ...result, [item.age]: [item.name] };
+      let age = item.age;
+      if (!result[age]) {
+        result[age] = [];
       }
+      result[age].push(item.name);
+      console.log(result);
+      return result;
     }, {});
 }
 const group = parsing();
 console.log(group);
-
-// 배열을 객체로 변환
-const qs = "banana=10&apple=20&orange=30";
-function parse(qs) {
-  const queryString = qs.substr(0);
-  const chunks = queryString.split("&");
-  return chunks
-    .map((chunk) => {
-      const [key, value] = chunk.split("=");
-      return { key, value };
-    })
-    .reduce(
-      (result, item) => ({
-        ...result,
-        [item.key]: item.value,
-      }),
-      {}
-    );
-}
-const params = parse(qs);
-console.log(params);
