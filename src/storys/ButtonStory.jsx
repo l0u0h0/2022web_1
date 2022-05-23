e2082b927269d4051712f1f6554dca9f078d7236
l0u0h0 <l0u0h0@gmail.com>
2022-05-23 23:49:57 +0900

    13weeks_2.5

--- src/storys/ButtonStory.jsx
+++ src/storys/ButtonStory.jsx
@@ -6,6 +6,11 @@ storiesOf("Button", module)
   .addWithJSX("기본 설정", () => <Button>전송하기</Button>)
   .addWithJSX("disabled", () => <Button disabled>전송하기</Button>)
   .addWithJSX("large", () => <Button large>전송하기</Button>)
+  .addWithJSX("xlarge", () => <Button xlarge>전송하기</Button>)
+  .addWithJSX("small", () => <Button small>전송하기</Button>)
+  .addWithJSX("xsmall", () => <Button xsmall>전송하기</Button>)
+  .addWithJSX("primary", () => <Button primary>전송하기</Button>)
+  .addWithJSX("secondary", () => <Button secondary>전송하기</Button>)
   .addWithJSX("primary, large", () => (
     <Button primary large>
       전송하기
