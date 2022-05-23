a0aeff7c680e624ac161c9a2d0339a70bf409705
l0u0h0 <l0u0h0@gmail.com>
2022-05-23 14:08:16 +0900

    13weeks_1

--- src/storys/TextStory.jsx
+++ src/storys/TextStory.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import Text from "../12/12_4";
+import Text from "../12/Text";
 import Page1 from "../12/Page";
 
 storiesOf("Text", module)
   .addWithJSX("기본 설정", () => <Text>안녕하세요</Text>)
-  .addWithJSX("PageStory", () => <Page1 />);
+  .addWithJSX("PageStory", () => <Page1 />)
+  .addWithJSX("large 예제", () => <Text large>안녕하세요</Text>)
+  .addWithJSX("xlarge 예제", () => <Text xlarge>안녕하세요</Text>)
+  .addWithJSX("small 예제", () => <Text small>안녕하세요</Text>)
+  .addWithJSX("xsmall 예제", () => <Text xsmall>안녕하세요</Text>)
+  .addWithJSX("primary 예제", () => <Text primary>안녕하세요</Text>)
+  .addWithJSX("secondary 예제", () => <Text secondary>안녕하세요</Text>)
+  .addWithJSX("pri, lar 예제", () => (
+    <Text primary large>
+      안녕하세요
+    </Text>
+  ));
