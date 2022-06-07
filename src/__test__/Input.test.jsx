a7880319eec6b50a3f88c715fd0b1da5e00b2906
l0u0h0 <l0u0h0@gmail.com>
2022-06-07 21:15:56 +0900

    14weeks_2

--- src/__test__/Input.test.jsx
+++ src/__test__/Input.test.jsx
@@ -1,12 +1,30 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Input from "../12/12_2";
+
+// describe("<Input>", () => {
+//   it("renders without crashing", () => {
+//     const div = document.createElement("div");
+//     ReactDOM.render(<Input name="name" />, div);
+//     ReactDOM.unmountComponentAtNode(div);
+//     expect(React.isValidElement(<Input name="name" />)).toBeTruthy();
+//   });
+// });
+
+// enzyme
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 import Input from "../12/12_2";
 
 describe("<Input>", () => {
   it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Input name="name" />, div);
-    ReactDOM.unmountComponentAtNode(div);
-    expect(React.isValidElement(<Input name="name" />)).toBeTruthy();
+    expect(() => {
+      shallow(<Input name="name" />);
+    }).not.toThrow();
+  });
+  it("has one element", () => {
+    const wrapper = shallow(<Input name="test_name" />);
+    expect(wrapper.length).toEqual(1);
+    expect(wrapper).toHaveLength(1);
   });
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,19 @@
 // src/setupTests.js
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16.3";
+import { jsxDecorator } from "storybook-addon-jsx";
 
 configure({ adapter: new Adapter() });
+
+afterEach(() => {
+  // console.error() 함수 객체에 spyOn() 함수로 추가된 가상 코드 제거
+  console.error.mockClear();
+});
+
+beforeEach(() => {
+  jest.spyOn(console, "error").mockImplementation((e) => {
+    // spyOn을 사용하여 console 객체의
+    // error() 함수를 실제 기능 대신 mockImplementation에 전달되는 함수가 실행되도록 설정
+    throw new Error(e); // console.error() 실행될 때 절달된 인자로 오류를 발생하도록 함.
+  });
+});
